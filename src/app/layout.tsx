f44cf10d7433387367a6dc4b8cafc5fa025fc409
neann-abdill <habdill606@gmail.com>
2026-09-15 14:19:00 +0700

    fix: use favicon ico as browser icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,14 +34,7 @@ export const metadata: Metadata = {
   publisher: "Pesantren Anshorussunnah",
   applicationName: "Anshorussunnah LMS",
   icons: {
-    icon: [
-      { url: "/favicon.ico", sizes: "any" },
-      { url: "/icon-48x48.png", sizes: "48x48", type: "image/png" },
-      { url: "/icon-96x96.png", sizes: "96x96", type: "image/png" },
-      { url: "/icon-144x144.png", sizes: "144x144", type: "image/png" },
-      { url: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
-      { url: "/icon-512x512.png", sizes: "512x512", type: "image/png" },
-    ],
+    icon: [{ url: "/favicon.ico", sizes: "any", type: "image/x-icon" }],
     shortcut: "/favicon.ico",
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
